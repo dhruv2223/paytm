afc6473e505b1147c2fc4c2d80d8ec82a52838a4
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2024-08-05 13:25:30 +0530

    created the signup route in user.js

--- backend/db.js
+++ backend/db.js
@@ -33,4 +33,4 @@ const userSchema= new Schema({
 });
 
 const User = mongoose.model('User',userSchema);
-export default User;
+export User;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,44 @@
 import express from "express"; 
+import zod from "zod";
+import {User} from "../db.sj" ;
+const jwt = "";
+const signupSchema = zod.object({
+  username:zod.string(),
+  firstName:zod:string(),
+  lastName:zod.string(),
+  password:zod.string()
+});
 
 const app = express();
 const router = express.Router();
-router.get("/singup",(req,res)=>{
-  
-});
+router.post("/singup",async (req,res)=>{
+ const body = req.body;
+  const {success} = signupSchema.safeParse(body);
+  if(!success){
+    res.status(400).json({message:"Email already taken/Incorrect Inputs "});
+  }
+  const userExists = User.findOne({
+    username:body.username;
+  }) 
+  if(userExists){
+  return res.status(400).json({message:"Email already taken/Incorrect Inputs "});
+  }
+  const user = await User.create({
+    username:body.username,
+    password:body.password,
+    firstname:body.firstname, 
+    lastName:body.lastName, 
+
+  })
+  const userId = user_.id;
+  const token = jwt.sign({
+    userId
+  },jwt);
+  res.json({message:"user created succesfully",token});
+}); 
+router.get("signin",(req,res)=>{
+
+})
 
 
 export default router;
